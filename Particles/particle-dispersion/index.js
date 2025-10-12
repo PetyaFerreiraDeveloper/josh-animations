c03446e9d9b7880913e5fd99bad233d4e294e83d
Petya Naydenova Ferreira <pn@abtion.com>
2025-10-12 09:56:08 +0200

    particles start from center and disperse randomly

---
+++ Particles/particle-dispersion/index.js
@@ -0,0 +1,39 @@
+// Simple random percentage function
+function randomPercent() {
+  return Math.random() * 100;
+}
+
+const button = document.querySelector(".particleButton");
+
+button.addEventListener("click", function () {
+  button.classList.toggle("liked");
+  const isLiked = button.classList.contains("liked");
+
+  // Bail out early if the user is *undoing* their like.
+  if (!isLiked) return;
+
+  // Create fadeout duration constant to control particle lifespan in one place
+  const FADE_OUT_DURATION = 1000; // in ms
+  
+  const particles = [];
+
+  // Create multiple particles
+  const numberOfParticles = 5;
+  Array(numberOfParticles).fill().forEach((_, index) => {
+    const particle = document.createElement("span");
+    particle.classList.add("particle");
+
+    particle.style.top = randomPercent() + "%";
+    particle.style.left = randomPercent() + "%";
+    particle.style.setProperty("--fade-duration", `${FADE_OUT_DURATION}ms`);
+
+    button.appendChild(particle);
+    particles.push(particle);
+  });
+
+  // Remove particles after animation duration
+  window.setTimeout(() => {
+    particles.forEach(particle => particle.remove())
+  }, FADE_OUT_DURATION + 200)
+
+});
